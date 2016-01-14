5719a945402475468776fd8f1c0b643af661d5ec
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-01-14 00:38:41 +0000

    have to revisit to make it work properly

--- contact.rb
+++ contact.rb
@@ -3,10 +3,15 @@ require 'csv'
 # Represents a person in an address book.
 class Contact
 
-  attr_accessor :name, :email
+  attr_accessor :name, :email, :phone_numbers
 
   def initialize(name, email)
-    # TODO: Assign parameter values to instance variables.
+    @name = name
+    @email = email
+  end
+
+  def contact_info_to_string
+    "#{name} #{email}"
   end
 
   # Provides functionality for managing a list of Contacts in a database.
@@ -15,21 +20,34 @@ class Contact
     # Returns an Array of Contacts loaded from the database.
     def all
       # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
+      CSV.read("data.csv").map do | row |
+        Contact.new(row[0], row[1])
+      end
+
     end
 
     # Creates a new contact, adding it to the database, returning the new contact.
-    def create(name, email)
+    def create(name, email, phone_numbers = {})
       # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
+       CSV.open('data.csv', 'a') do | csv |                   #come back to this so it creates a new line when there isn't a blank one
+          csv << [name, email, phone_numbers]
+        end
+
+       # existing_list << Contact.new(name, email)
     end
 
     # Returns the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
       # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
+      CSV.read("data.csv")[id.to_i - 1]         #Can I do this without reading whole file???
     end
 
     # Returns an array of contacts who match the given term.
     def search(term)
       # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
+      CSV.foreach("data.csv") do | row |
+        return row if row.any? { |ele| ele =~ /#{term}/i }
+      end
     end
 
   end
--- contact_list.rb
+++ contact_list.rb
@@ -1,8 +1,94 @@
 require_relative 'contact'
+require 'pry'
 
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
 
+  attr_accessor :user_input
+
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
+  def menu
+    puts "Here is a list of available commands:"
+    puts "\tnew\t- Create a new contact"
+    puts "\tlist\t- List all contacts"
+    puts "\tshow\t- Show a contact"
+    puts "\tsearch\t- Search contacts"
+    @user_input = gets.chomp
+  end
+
+  def list
+      num = 0
+    Contact.each do | row |
+      num += 1
+      puts "#{num}: #{row[0]}   #{row[1]}   #{row[2]}"
+    end
+  end
+
+  def new_contact
+    temp_numbers = {}
+    puts "Enter name (first and last):"
+    name = gets.chomp
+    puts "Enter email:"
+    email = gets.chomp
+    puts "Add phone numbers? 'yes' or 'no'"
+    enter_numbers = gets.chomp
+    if enter_numbers == "yes"
+      loop do
+        puts "Enter number label ('home', 'mobile', etc):"
+        label = gets.chomp.to_sym
+        puts "Enter the phone number:"
+        number = gets.chomp
+        temp_numbers[label] = number
+        puts "Add another number? 'yes' or 'no'"
+        answer = gets.chomp.downcase
+        break if answer == "no"
+      end
+    end
+    if check_email(email)
+      puts "A contact with that email already exists. Contact was not created."
+    else
+      Contact.create(name, email, temp_numbers)
+    end
+  end
+
+  def check_email(new_email)
+    CSV.foreach("data.csv") do |row|
+      return true if row.any? { |ele| ele =~ /#{new_email}/i }
+    end
+  end
+
+
+
+  def find_by_id
+    puts "Enter ID number:"
+    id = gets.chomp
+    puts Contact.find(id)[0]
+    puts Contact.find(id)[1]
+  end
+
+  def find_by_search_term
+    puts "What do you want to search for?"
+    search_term = gets.chomp
+    puts "#{CSV.read("data.csv").find_index(Contact.search(search_term)) + 1}. #{Contact.search(search_term).join("   ")}"
+  end
 
 end
+
+def start_list
+  new_list = ContactList.new
+  new_list.menu
+  case new_list.user_input
+  when "list"
+    new_list.list
+  when "new"
+    new_list.new_contact
+  when "show"
+    new_list.find_by_id
+  when "search"
+    new_list.find_by_search_term
+  end
+   
+
+end
+
+start_list
