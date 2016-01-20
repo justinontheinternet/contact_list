885aab983fa831fe9fe8b0b902614a909036810a
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-01-20 00:09:30 +0000

    ORM version

--- contact.rb
+++ contact.rb
@@ -1,60 +1,67 @@
-require 'csv'
-
 # Represents a person in an address book.
 class Contact
 
   attr_accessor :name, :email, :phone_numbers
+  attr_reader :id
 
-  @@file = CSV.read("data.csv")
 
-  def initialize(name, email, phone_numbers = {})
+  def initialize(name, email, id=nil)
+    @id = id
     @name = name
     @email = email
   end
 
-  def info_to_string
-    if self.phone_numbers != nil
-      "#{name} (#{email}) (#{@phone_numbers})"
+  def save
+    if self.id == nil
+      result = self.class.connection.exec_params('INSERT INTO contacts (name, email) VALUES ($1, $2);', [self.name, self.email])
     else
-      "#{name} (#{email})"
+      result = self.class.connection.exec_params('UPDATE contacts SET name = $1, email = $2 WHERE id = $3::int;', [self.name, self.email, self.id.to_i])
     end
+  end
+
+  def destroy
+    binding.pry
+    Contact.connection.exec_params('DELETE FROM contacts WHERE id = $1', [self.id])
   end
 
   # Provides functionality for managing a list of Contacts in a database.
   class << self
 
+    def connection
+      @@connection ||= PG.connect(dbname: 'contact_orm')
+    end
+
     # Returns an Array of Contacts loaded from the database.
     def all
-      # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
-      @@file.map do | row |
-        contact = Contact.new(row[0], row[1])
-        "#{@@file.index(row) + 1}. " + contact.info_to_string
+      all_contacts = []
+      result = connection.exec('SELECT * FROM contacts;')
+      result.each do |each_contact|     #result = an array of hashes. Each hash is a contact/ro
+        contact = Contact.new(
+          each_contact['name'],
+          each_contact['email'],
+          each_contact['id']
+          )
+        all_contacts << contact         #push each contact (now an instance/object), into the all contacts array
       end
+      all_contacts                      #return the array full on instances
     end
 
     # Creates a new contact, adding it to the database, returning the new contact.
-    def create(name, email, phone_numbers = {})
-      # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
-       CSV.open('data.csv', 'a') do | csv |    #come back to this so it creates a new line when there isn't a blank one
-          csv << [name, email, phone_numbers]
-        end
-    end
+    # def create(new_contact)
+    #   new_contact.save
+    # end
 
-    # Returns the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
-      # TODO: Find the Contact in the 'contacts.csv' file with the matching id.  
-      @@file[id.to_i - 1]         #Can I do this without reading whole file???
-    end
-
-    # Returns an array of contacts who match the given term.
-    def search(term)
-      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
-      CSV.foreach("data.csv") do | row |
-        binding.pry
-        return row if row.any? { |ele| ele =~ /#{term}/i }
+      result = connection.exec_params('SELECT id, name, email FROM contacts WHERE id = $1::int;', [id])
+      if row = result.first
+        Contact.new(row['name'], row['email'], row['id'])
+      else
+        nil
       end
     end
 
+    def search(term)
+      result = connection.exec_params("SELECT * FROM contacts WHERE LOWER(name) LIKE LOWER('%' || $1 || '%') OR LOWER(email) LIKE LOWER('%' || $1 || '%');", [term])
+    end
   end
-
 end
--- contact_list.rb
+++ contact_list.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+require 'pg'
 require_relative 'contact'
 require 'pry'
 
@@ -15,25 +16,32 @@ class ContactList
       new_list.list
     when "new"
       new_list.new_contact
+    when "update"
+      new_list.update_contact
     when "show"
       new_list.find_by_id
     when "search"
       new_list.find_by_search_term
+    when "delete"
+      new_list.delete_contact
     end
   end
 
-  # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
   def menu
     puts "Here is a list of available commands:"
     puts "\tnew\t- Create a new contact"
+    puts "\tupdate\t- Update contact info"
     puts "\tlist\t- List all contacts"
     puts "\tshow\t- Show a contact"
     puts "\tsearch\t- Search contacts"
+    puts "\tdelete\t- Delete contact"
     @user_input = gets.chomp
   end
 
   def list
-    puts Contact.all
+    (Contact.all).each do |contact|
+      puts "#{contact.id}. #{contact.name}  (#{contact.email})"
+    end
   end
 
   def new_contact
@@ -42,26 +50,47 @@ class ContactList
     name = gets.chomp
     puts "Enter email:"
     email = gets.chomp
-    puts "Add phone numbers? 'yes' or 'no'"
-    enter_numbers = gets.chomp
-    if enter_numbers == "yes"
-      loop do
-        puts "Enter number label ('home', 'mobile', etc):"
-        label = gets.chomp.to_sym
-        puts "Enter the phone number:"
-        number = gets.chomp
-        temp_numbers[label] = number
-        puts "Add another number? 'yes' or 'no'"
-        answer = gets.chomp.downcase
-        break if answer == "no"
-      end
-    end
-    if check_email(email)
-      puts "A contact with that email already exists. Contact was not created."
-    else
-      Contact.create(name, email, temp_numbers)
+    # puts "Add phone numbers? 'yes' or 'no'"
+    # enter_numbers = gets.chomp
+    # if enter_numbers == "yes"
+    #   loop do
+    #     puts "Enter number label ('home', 'mobile', etc):"
+    #     label = gets.chomp.to_sym
+    #     puts "Enter the phone number:"
+    #     number = gets.chomp
+    #     temp_numbers[label] = number
+    #     puts "Add another number? 'yes' or 'no'"
+    #     answer = gets.chomp.downcase
+    #     break if answer == "no"
+    #   end
+    # end
+    # if check_email(email)
+    #   puts "A contact with that email already exists. Contact was not created."
+    # else
+      new_contact = Contact.new(name, email)
+      new_contact.save
       puts "Contact was successfully added."
-    end
+    # end
+  end
+
+  def update_contact
+    puts "Enter the contact id you'd like to update."
+    id = gets.chomp
+    existing_contact = Contact.find(id)
+    puts "Enter new name:"
+    existing_contact.name = gets.chomp
+    puts "Enter new email:"
+    existing_contact.email = gets.chomp
+    existing_contact.save
+    puts "Contact updated."
+  end
+
+  def delete_contact
+    puts "Enter the contact id you'd like to delete."
+    id = gets.chomp
+    existing_contact = Contact.find(id)
+    existing_contact.destroy
+    puts "Contact terminated."
   end
 
   def check_email(new_email)
@@ -73,17 +102,18 @@ class ContactList
   def find_by_id
     puts "Enter ID number:"
     id = gets.chomp
-    puts Contact.find(id)[0]
-    puts Contact.find(id)[1]
+    contact = Contact.find(id)
+    puts "#{contact.id}. #{contact.name}  (#{contact.email})"
   end
 
   def find_by_search_term
     puts "What do you want to search for?"
     search_term = gets.chomp
-    puts "#{CSV.read("data.csv").find_index(Contact.search(search_term)) + 1}. #{Contact.search(search_term).join("   ")}"
+    search_result = Contact.search(search_term)
+    search_result.each do |contact|
+      puts "#{contact["id"]}. #{contact["name"]}  (#{contact["email"]})"
+    end
   end
-   
-
 end
 
 ContactList.create_list
